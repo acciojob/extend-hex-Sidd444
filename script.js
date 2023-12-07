0e19df01e964e082052994cc1e8b25e54711387c
Sidd444 <123827703+Sidd444@users.noreply.github.com>
2023-12-07 15:08:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let s="#";
+	s+=shortHex[1]+shortHex[1];
+	s+=shortHex[2]+shortHex[2];
+	s+=shortHex[3]+shortHex[3];
+	return s;
 };
 
 // Do not change the code below.
