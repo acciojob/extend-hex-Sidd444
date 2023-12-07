c624f3c657614af5dbbe664fab8c6cc3954fba3a
Sidd444 <123827703+Sidd444@users.noreply.github.com>
2023-12-07 15:52:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const extendHex = (shortHex) => {
   // write your code here
 	let s="#";
-	s+=shortHex[1]+shortHex[1];
-	s+=shortHex[2]+shortHex[2];
-	s+=shortHex[3]+shortHex[3];
+	if(shortHex.length===4){
+	 s+=shortHex[1]+shortHex[1];
+	 s+=shortHex[2]+shortHex[2];
+	 s+=shortHex[3]+shortHex[3];
+	}else{
+		s+=shortHex[0]+shortHex[0];
+		s+=shortHex[1]+shortHex[1];
+		s+=shortHex[2]+shortHex[2];
+	}
+	
 	return s;
 };
-
+   
 // Do not change the code below.
-const shortHex = prompt("Enter Short Hex.");
+//const shortHex = prompt("Enter Short Hex.");
 alert(extendHex(shortHex));
